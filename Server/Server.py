df132c617324050b94c0e814fcaf95ee2d1af526
liangdalong <liangdalong2010@gmail.com>
2018-10-16 14:30:30 +0800

    update Server.py

--- Server/Server.py
+++ Server/Server.py
@@ -12,3 +12,26 @@ print(host)
 port = 5900
 
 server_addr = (host,port)
+
+
+inputs = []
+
+fd_name = {}
+
+
+
+def serverInit():
+    ss = socket.socket()
+    ss.bind(server_addr)
+    se.listen(10)
+
+    return ss
+
+def newConnection(ss):
+    client_conn,client_addr = ss.accept()
+    try:
+        client_conn.send("welcome to chatroom.pls set up your nick name!")
+        
+
+    except Exception as e:
+        print(e)
