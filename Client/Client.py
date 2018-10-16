811393e5fc2796a8b5f6cc49734097b46e23f9f8
liangdalong <liangdalong2010@gmail.com>
2018-10-16 14:03:55 +0800

    update Client

--- Client/Client.py
+++ Client/Client.py
@@ -9,3 +9,54 @@ host = ''
 port = 9000
 
 client_addr = (host,port)
+
+#倾听其他成员谈话
+
+def listening(cs):
+    inputs = [cs]
+    rlist,wlist,elist = select.select(inputs, [] , [])
+    if cs in rlist:
+        try:
+            print(cs.recv(1024))
+        except secket.error:
+            print("socket is error")
+            exit()
+
+def speak(cs):
+    while True:
+        try:
+            data = raw_input()
+        except Exception as e:
+            print("can't input")
+            exit()
+
+
+        try:
+            cs.send(data)
+        except Exception as e:
+            exit()
+
+def main():
+
+    cs = socket.socket()
+    cs.connect(client_addr)
+
+    t = threading.Thread(target = listening,args = (cs,))
+    t.start()
+
+    t1 = threading.Thread(target = speak,args = (cs,))
+    t1.start()
+
+if __name__ == "__main__":
+    main()
+
+
+    
+
+
+
+
+
+
+    
+        
